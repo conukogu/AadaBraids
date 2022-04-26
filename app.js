2bfb5e4e7e9d134eaeea0760c69ac3dd3cef920e
Chioma Onukogu <conukogu1@gmail.com>
2022-04-25 20:29:26 -0400

    trying to add db

--- app.js
+++ app.js
@@ -1,32 +1,108 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser')
+// const mysql = require('mysql')
 
-const path = require('path');
+// const path = require('path');
+
+// const port = process.env.PORT || 3000;
+
+// const db_config = {
+//   host: "us-cdbr-east-05.cleardb.net",
+//   user: "bc778fd640b8ba",
+//   password: "78a7b63d",
+//   database: "heroku_f62ce51aa2ee177"
+// };
+
+// const db=mysql.createPool({ connectionLimit: 5, ...db_config})
+
+
+// app.use(express.static(path.join(__dirname, "/build")));
+
+
+// app.get("/api/getList", (req, res) => {
+//   const insertQ = "SELECT * FROM heroku_f62ce51aa2ee177.contactform;";
+//   db.query(insertQ, (err, result) => {
+//     res.send(result);
+//   });
+// });
+
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/build/index.html"));
+// });
+
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// app.post("/api/insert", (req, res) => {
+//   const firstName = req.body.firstName;
+//   const lastName = req.body.lastName;
+//   const email = req.body.email;
+//   const phoneNumber = req.body.phoneNumber;
+//   const message = req.body.message;
+
+//   const sqlInsert =
+//     "INSERT INTO heroku_f62ce51aa2ee177.contactform(firstName, lastName, email, phoneNumber, message) VALUES (?,?,?,?,?)";
+//   db.query(
+//     sqlInsert,
+//     [firstName, lastName, email, phoneNumber, message],
+//     (err, result) => {
+//       console.log(result);
+//     }
+//   );
+// });
+
+
+// app.listen(port, (err) =>{
+//     if (err) return console.log(err);
+//     console.log('Server running on port:', port)
+// })
+
+
+
+
+
+
+
+
+
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
+mysql://b33dec1088e7e0:6cf06160@us-cdbr-east-05.cleardb.net/heroku_6b8ae48b51df0de?
+
 const db_config = {
   host: "us-cdbr-east-05.cleardb.net",
-  user: "bc778fd640b8ba",
-  password: "78a7b63d",
-  database: "heroku_f62ce51aa2ee177"
+  user: "b33dec1088e7e0",
+  password: "6cf06160",
+  database: "heroku_6b8ae48b51df0de"
 };
 
-const db=mysql.createPool({ connectionLimit: 5, ...db_config})
-
+const db = mysql.createPool({connectionLimit: 5, ...db_config});
 
 app.use(express.static(path.join(__dirname, "/build")));
 
-
-app.get("/api/getList", (req, res) => {
-  const insertQ = "SELECT * FROM heroku_f62ce51aa2ee177.contactform;";
+app.get("/api/productList", (req, res) => {
+  const insertQ = "SELECT * FROM heroku_6b8ae48b51df0de.product_info;";
   db.query(insertQ, (err, result) => {
     res.send(result);
   });
 });
 
+// app.get("/api/formList", (req, res) => {
+//   const insertQ = "SELECT * FROM heroku_6b8ae48b51df0de.form_info;";
+//   db.query(insertQ, (err, result) => {
+//     res.send(result);
+//   });
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
@@ -35,7 +111,7 @@ app.get("*", (req, res) => {
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/api/insert", (req, res) => {
+app.post("/api/formInsert", (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
@@ -43,7 +119,7 @@ app.post("/api/insert", (req, res) => {
   const message = req.body.message;
 
   const sqlInsert =
-    "INSERT INTO heroku_f62ce51aa2ee177.contactform(firstName, lastName, email, phoneNumber, message) VALUES (?,?,?,?,?)";
+    "INSERT INTO heroku_6b8ae48b51df0de.form_info(firstName, lastName, email, phoneNumber, message) VALUES (?,?,?,?,?)";
   db.query(
     sqlInsert,
     [firstName, lastName, email, phoneNumber, message],
@@ -53,83 +129,7 @@ app.post("/api/insert", (req, res) => {
   );
 });
 
-
-app.listen(port, (err) =>{
-    if (err) return console.log(err);
-    console.log('Server running on port:', port)
-})
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const mysql = require("mysql");
-
-// const path = require("path");
-
-// const port = process.env.PORT || 3000;
-
-// mysql://b33dec1088e7e0:6cf06160@us-cdbr-east-05.cleardb.net/heroku_6b8ae48b51df0de?
-
-// const db_config = {
-//   host: "us-cdbr-east-05.cleardb.net",
-//   user: "b33dec1088e7e0",
-//   password: "6cf06160",
-//   database: "heroku_6b8ae48b51df0de",
-// };
-
-// const db = mysql.createPool({connectionLimit: 5, ...db_config});
-
-// app.use(express.static(path.join(__dirname, "/build")));
-
-// app.get("/api/productList", (req, res) => {
-//   const insertQ = "SELECT * FROM heroku_6b8ae48b51df0de.product_info;";
-//   db.query(insertQ, (err, result) => {
-//     res.send(result);
-//   });
-// });
-
-// // app.get("/api/formList", (req, res) => {
-// //   const insertQ = "SELECT * FROM heroku_6b8ae48b51df0de.form_info;";
-// //   db.query(insertQ, (err, result) => {
-// //     res.send(result);
-// //   });
-// // });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/build/index.html"));
-// });
-
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// // app.post("/api/formInsert", (req, res) => {
-// //   const firstName = req.body.firstName;
-// //   const lastName = req.body.lastName;
-// //   const email = req.body.email;
-// //   const phoneNumber = req.body.phoneNumber;
-// //   const message = req.body.message;
-
-// //   const sqlInsert =
-// //     "INSERT INTO heroku_6b8ae48b51df0de.form_info(firstName, lastName, email, phoneNumber, message) VALUES (?,?,?,?,?)";
-// //   db.query(
-// //     sqlInsert,
-// //     [firstName, lastName, email, phoneNumber, message],
-// //     (err, result) => {
-// //       console.log(result);
-// //     }
-// //   );
-// // });
-
-// app.listen(port, (err) => {
-//   if (err) return console.log(err);
-//   console.log("Server running on port:", port);
-// });
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log("Server running on port:", port);
+});
